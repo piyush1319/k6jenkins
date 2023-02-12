c3a60d221485537e4a85a59e63d1a16c752e1702
piyush1319 <32597204+piyush1319@users.noreply.github.com>
2023-02-12 18:03:10 +0530

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ex
 
-pnpm apt-get update
-pnpm apt-get install dirmngr --install-recommends
-pnpm apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
-pnpm apt-get update
-pnpm apt-get install k6
-jenkins ALL= NOPASSWD: ALL
+sudo apt-get update
+sudo apt-get install dirmngr --install-recommends
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install k6
